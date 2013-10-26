a77fb884d37432d492603bfb9b7e98579b8e51b7
ibigbug <xiaobayuwei@gmail.com>
2013-10-26 20:15:37 +0800

    jshintrc

---
+++ jshintrc.js
@@ -0,0 +1,68 @@
+{
+    // Enforing
+    "bitwise": false,
+    "camelcase": false,
+    "curly": false,
+    "eqeqeq": true,
+    "es3": true,
+    "forin": true,
+    "freeze": true,
+    "immed": true,
+    "indent": 2,
+    "latedef": "nofunc",
+    "newcap": true,
+    "noarg": true,
+    "noempty": true,
+    "nonew": true,
+    "plusplus": false,
+    "quotmark": true,
+    "undef": true,
+    "unused": true,
+    "strict": false,
+    "trailing": true,
+    //"maxparams": 3,
+    "maxdepth": 3,
+    //"maxstatements": 4,
+    //"maxcomplexity": 3,
+    "maxlen": 80,
+
+    // Relaxing
+    "asi": false,
+    "boss": false,
+    "debug": false,
+    "eqnull": false,
+    "esnext": false,
+    "evil": false,
+    "expr": false,
+    "funcscope": false,
+    "globalstrict": false,
+    "iterator": false,
+    "lastsemic": true,
+    "laxbreak": false,
+    "laxcomma": false,
+    "loopfunc": false,
+    "moz": false,
+    "multistr": false,
+    "notypeof": false,
+    "proto": false,
+    //"scripturl": true,
+    "smarttabs": false,
+    "shadow": false,
+    "sub": false,
+    "supernew": false,
+    
+    // Environments
+    "browser": true,
+    "devel": false,
+    "node": false,
+
+    //Legacy
+    "nomen": true,
+    "onevar": false,
+    "passfail": false,
+    "white": true,
+
+    // Undocumented
+    "maxerr": 100,
+    "predef": ["angular"]
+}
